a65b358021043e1c6bbb1df93b76e5deee5db133
Gustin Schumacher <97652359+gustinCodes@users.noreply.github.com>
2022-04-14 15:04:26 -0500

    added javascript

--- Resources/script/script.js
+++ Resources/script/script.js
@@ -1,9 +1,29 @@
 const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listdisplayContiner = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector()
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTD_LIST_ID_KEY = "task.selectedListId";
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTD_LIST_ID_KEY);
+
+listsContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListButton.addEventListener("click", (e) => {
+  lists = lists.filter((list) => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
 
 newListForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -26,6 +46,7 @@ function saveAndRender() {
 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTD_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
@@ -35,6 +56,9 @@ function render() {
     listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
     listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list");
+    }
     listsContainer.appendChild(listElement);
   });
 }
